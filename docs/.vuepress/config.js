dcbdd53e218b7bb1b60ac0c6fa9afb5b73c90fa7
miracletjf <1398988546@qq.com>
2021-08-12 00:53:58 +0800

    chore: 添加别名配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,16 @@
+const path = require('path')
+
 module.exports = {
   title: 'Hello VuePress',
-  description: 'Just playing around'
+  description: 'Just playing around',
+  themeConfig: {
+    logo: '/assets/img/logo.png',
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@images': path.join(__dirname, 'assets/images')
+      }
+    }
+  }
 }
