a81196e14e5a16fa1afd912eb4344e0af86fe1cc
miracletjf <1398988546@qq.com>
2021-08-12 23:04:20 +0800

    chore: 配置菜单栏结构

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,12 +4,13 @@ module.exports = {
   title: 'Hello VuePress',
   description: 'Just playing around',
   themeConfig: {
-    logo: 'hero.png',
+    logo: '/hero.png',
     // navbar: false,  // 禁用导航条
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'TypeScript', link: '/typescript/' },
       { text: 'Node', link: '/node/' },
+      { text: 'TypeScript', link: '/typescript/' },
+      { text: 'Vue', link: '/vue/' },
       // { text: 'Dropdown',  items: [
       //   { text: 'xxx', link: 'xxx' },
       //   { text: 'items', items: [
@@ -21,12 +22,42 @@ module.exports = {
       // ]}
       // { text: 'GitHub', link: '' }
     ],
-    displayAllHeaders: true,
-    sidebar: [
-      '/node/',
-      '/typescript/',
-      ['/vue/', 'Explicit link text']
-    ]
+    // displayAllHeaders: true,
+    sidebar: {
+      '/node/': [{
+        title: 'Node',   // 必要的
+        // path: '/node/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+          ['/node/', 'NODE'],
+          ['/typescript/', 'TYPESCRIPT'],
+          ['/vue/', 'VUE']
+        ]
+      }],
+      '/typescript/': [{
+        title: 'Typescript',   // 必要的
+        // path: '/node/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+          ['/node/', 'NODE'],
+          ['/typescript/', 'TYPESCRIPT'],
+          ['/vue/', 'VUE']
+        ]
+      }],
+      '/vue/': [{
+        title: 'Vue',   // 必要的
+        // path: '/node/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+          ['/node/', 'NODE'],
+          ['/typescript/', 'TYPESCRIPT'],
+          ['/vue/', 'VUE']
+        ]
+      }],
+    }
   },
   configureWebpack: {
     resolve: {
